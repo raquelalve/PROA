b511f507a3b0a587b015034b50a8efe5387e9787
raquelalve <raquelalves8367@gmail.com>
2026-06-14 23:16:58 -0300

    Mudanças nas imagens

---
+++ Java/TáservidoQ12.java
@@ -0,0 +1,43 @@
+public class TelefoneCelular {
+    private string marca;
+    private string modelo;
+    private int armazenamentoGB;
+    private double preco;
+    private boolean tem5G;
+
+    // Getters e Setters
+    public string getMarca() { return marca; }
+    public void setMarca(String marca) { this.marca = marca; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public int getArmazenamentoGB() { return armazenamentoGB; }
+    public void setArmazenamentoGB(int armazenamentoGB) { this.armazenamentoGB = armazenamentoGB; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
+
+    public boolean isTem5G() { return tem5G; }
+    public void setTem5G(boolean tem5G) { this.tem5G = tem5G; }
+}
+
+public class Main {
+    public static void main(string[] args) {
+        TelefoneCelular meuCelular = new TelefoneCelular();
+
+        // Usando setters para atribuir valores
+        meuCelular.setMarca("Samsung");
+        meuCelular.setModelo("Galaxy S24");
+        meuCelular.setArmazenamentoGB(256);
+        meuCelular.setPreco(4500.00);
+        meuCelular.setTem5G(true);
+
+        // Exibindo usando getters
+        System.out.println("Marca: " + meuCelular.getMarca());
+        System.out.println("Modelo: " + meuCelular.getModelo());
+        System.out.println("Armazenamento: " + meuCelular.getArmazenamentoGB() + "GB");
+        System.out.println("Preço: R$" + meuCelular.getPreco());
+        System.out.println("Possui 5G: " + (meuCelular.isTem5G() ? "Sim" : "Não"));
+    }
+}
--- README.md
+++ README.md
@@ -4,18 +4,8 @@
   <img src="./Imagensreadme/logo nome.png" alt="Acessar o site do PROA" width="100" >
 </a>
 </p>
+
+<p align="center">
 O PróProfissão é um programa de qualificação profissional gratuito voltado para jovens, criado pelo Instituto PROA em parceria com instituições de ensino. O projeto oferece capacitação do zero até o ingresso no mercado de tecnologia, ajudando a conectar os alunos formados com empresas parceiras, oferecendo formação técnica e desenvolvimento de habilidades comportamentais (soft skills) para facilitar o primeiro emprego.
+</p>
 
-##  Linguagens
-* HTML - CSS
-* Java
-* JavaScript 
-* React
-* SQL
-
-###  Tecnologias 
-
-
-
-#### 🛠️ Como executar o projeto
-Explique aqui como rodar o código no computador.
